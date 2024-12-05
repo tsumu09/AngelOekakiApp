957f208f1d82e1b3a60f067f0dda8cd48d7d2c39
urassh <shuto.1011@icloud.com>
2024-12-06 00:10:58 +0900

    feat: カメラ機能の実装

--- AngelOekakiApp/ViewController/ConfirmViewController.swift
+++ AngelOekakiApp/ViewController/ConfirmViewController.swift
@@ -2,14 +2,17 @@
 //  ConfirmViewController.swift
 //  AngelOekakiApp
 //
-//  Created by 浦山秀斗 on 2024/12/05.
+//  Created by 浦山秀斗 on 2024/12/06.
 //
 
 import UIKit
 
 class ConfirmViewController: UIViewController {
+    var image: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
 }
--- AngelOekakiApp/ViewController/ThemeSelectViewController.swift
+++ AngelOekakiApp/ViewController/ThemeSelectViewController.swift
@@ -8,8 +8,48 @@
 import UIKit
 
 class ThemeSelectViewController: UIViewController {
+    var capturedImage: UIImage?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    @IBAction func OnTapTakePhotoButton(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            let alert = UIAlertController(title: "Error", message: "Camera is not available", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            present(alert, animated: true)
+            return
+        }
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toConfirm" {
+            let destinationVC = segue.destination as? ConfirmViewController
+            destinationVC?.image = capturedImage!
+        }
+    }
+}
+
+extension ThemeSelectViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Finished picking media")
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        capturedImage = image
+        picker.dismiss(animated: true, completion: nil)
+        performSegue(withIdentifier: "toConfirm", sender: self)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+      picker.dismiss(animated: true, completion: nil)
+  }
 }
