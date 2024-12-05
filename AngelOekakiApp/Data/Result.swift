72dd8ec4c9e25096cf8d8fa6c69ae28941c24857
urassh <shuto.1011@icloud.com>
2024-12-05 23:26:25 +0900

    add: result

---
+++ AngelOekakiApp/Data/Result.swift
@@ -0,0 +1,15 @@
+//
+//  Result.swift
+//  AngelOekakiApp
+//
+//  Created by 浦山秀斗 on 2024/12/05.
+//
+
+import UIKit
+
+struct Result : Identifiable {
+    let id: Int
+    let image: UIImage
+    let resultText: String
+    let adviceText: String
+}
