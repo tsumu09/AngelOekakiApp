63d3a0044102d8d536397e5fc4e8ca0623b07605
urassh <shuto.1011@icloud.com>
2024-12-05 23:34:05 +0900

    add: protocol

---
+++ AngelOekakiApp/Model/Protocol/GetAdviceProtocol.swift
@@ -0,0 +1,10 @@
+//
+//  GetAdviceProtocol.swift
+//  AngelOekakiApp
+//
+//  Created by 浦山秀斗 on 2024/12/05.
+//
+
+protocol GetAdviceProtocol {
+    func getAdvice(point: Float, theme: Theme) -> String
+}
---
+++ AngelOekakiApp/Model/Protocol/GetPointProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  GetPointProtocol.swift
+//  AngelOekakiApp
+//
+//  Created by 浦山秀斗 on 2024/12/05.
+//
+
+import UIKit
+
+protocol GetPointProtocol {
+    func getPoint(image: UIImage, theme: Theme) -> String
+}
