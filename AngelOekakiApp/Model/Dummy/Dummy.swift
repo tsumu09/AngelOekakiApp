6043cfe5ecf9b64d910fbe7f4c9785e6ee50fd1c
urassh <shuto.1011@icloud.com>
2024-12-05 23:35:33 +0900

    add: Dummy

---
+++ AngelOekakiApp/Model/Dummy/Dummy.swift
@@ -0,0 +1,17 @@
+//
+//  Dummy.swift
+//  AngelOekakiApp
+//
+//  Created by 浦山秀斗 on 2024/12/05.
+//
+import UIKit
+
+class Dummy : GetPointProtocol, GetAdviceProtocol {
+    func getPoint(image: UIImage, theme: Theme) -> String {
+        "40点です"
+    }
+    
+    func getAdvice(point: Float, theme: Theme) -> String {
+        "データベースの物理設計と隣接する領域に、データのバックアップおよびリストアの設計があります。データベースは、システムにとって極めて重要なデータが一元管理されており、いわばシステムの心臓です。それゆえ、万が一にでもデータベース内のデータが失われるようなことがあってはなりません。もしそのようなことが起きた場合は、多額の損害を覚悟する必要があります。"
+    }
+}
